8eb4a70aa009a720e74cdd5ae5f89d6fbece64f2
Arthicsu <ronivl@yandex.ru>
2025-12-13 21:24:20 +0300

    - Added support for more than 1 image to upload/recognize - Changed design of the initial form and results (not final) - Bugfixes - Removed Herobrine

--- src/api/routers/pcb_defect_router.py
+++ src/api/routers/pcb_defect_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, File, UploadFile, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
 from src.services.logger import append_to_json_file
 
@@ -41,16 +41,13 @@ async def show_form(request: Request):
     })
 
 @router.post("/predict", response_class=HTMLResponse)
-async def predict_defect( #bro what. . .
+async def predict_defect(
         request: Request,
-        file: UploadFile = File(...),
+        file: list[UploadFile],
         threshold: float = Form(50.0),
         selected_classes: str = Form("")
     ):
     try:
-        file_bytes = await file.read()
-        nparr = np.frombuffer(file_bytes, np.uint8)
-        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         confidence_threshold = threshold / 100.0
 
         if selected_classes:
@@ -62,50 +59,68 @@ async def predict_defect( #bro what. . .
         else:
             selected_classes_list = []
 
-        results = model(img)[0]
+        results_for_template = []
 
-        detections = []
-        for box in results.boxes:
-            cls = int(box.cls[0])
-            conf = float(box.conf[0])
-            x1, y1, x2, y2 = map(int, box.xyxy[0])
-            defect_area = (x2 - x1) * (y2 - y1)
+        for uploaded_file in file:
+            file_bytes = await uploaded_file.read()
+            nparr = np.frombuffer(file_bytes, np.uint8)
+            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+            results = model(img)[0]
+            detections = []
+
+            for box in results.boxes:
+                cls = int(box.cls[0])
+                conf = float(box.conf[0])
+
+                if conf < confidence_threshold:
+                    continue
+
+                class_name = class_names[str(cls)].lower()
+                if selected_classes_list and class_name not in selected_classes_list:
+                    continue
+
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                defect_area = (x2 - x1) * (y2 - y1)
+
+                detections.append({
+                    "class_id": cls,
+                    "class_name": class_names[str(cls)],
+                    "confidence": round(conf, 3),
+                    "bbox": [x1, y1, x2, y2],
+                    "bbox_area": defect_area
+                })
 
 
-            detections.append({
-                "class_id": cls,
-                "class_name": class_names[str(cls)],
-                "confidence": round(conf, 3),
-                "bbox": [x1, y1, x2, y2],
-                "bbox_area": defect_area
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(img, f"{class_names[str(cls)]} {conf:.2f}",(x1, y1 - 5),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.7, (0, 255, 0), 2)
+
+            img_path = uploads_folder / f"pred_{uploaded_file.filename}"
+            cv2.imwrite(str(img_path), img)
+
+            log = {
+                "filename": uploaded_file.filename,
+                "threshold": confidence_threshold,
+                "selected_classes": selected_classes_list,
+                "img_path": str(img_path),
+                "detections": detections,
+                "timestamp": datetime.utcnow().isoformat()
+            }
+            append_to_json_file("static/logs/detections_logs.json", log)
+
+            results_for_template.append({
+                "filename": uploaded_file.filename,
+                "image_url": str(img_path),
+                "detections": detections
             })
-
-
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(img, f"{class_names[str(cls)]} {conf:.2f}",(x1, y1 - 5),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        0.7, (0, 255, 0), 2)
-
-        img_path = uploads_folder / f"pred_{file.filename}"
-        cv2.imwrite(str(img_path), img)
-
-        log = {
-            "filename": file.filename,
-            "threshold": confidence_threshold,
-            "selected_classes": selected_classes_list,
-            "img_path": str(img_path),
-            "detections": detections,
-            "timestamp": datetime.utcnow().isoformat()
-        }
-
-        append_to_json_file("static/logs/detections_log.json", log)
 
         return templates.TemplateResponse("pcb_defect_result.html", {
             "request": request,
-            "image_url": img_path,
             "threshold": confidence_threshold,
             "selected_classes": selected_classes_list,
-            "detections": detections,
+            "results": results_for_template,
             "title": "Результат детектирования"
         })
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,46 @@
-document.addEventListener(`DOMContentLoaded`, () => {
-    console.log(`Вы что-то тут хотите увидеть?)`)
+function addClassToInput(className) {
+    const input = document.getElementById('selected_classes');
+    const classes = input.value
+        .split(',')
+        .map(c => c.trim())
+        .filter(Boolean);
+
+    if (!classes.includes(className)) {
+        classes.push(className);
+        input.value = classes.join(', ');
+    }
+}
+
+const clsInput = document.getElementById('selected_classes');
+
+clsInput.addEventListener('focus', function() {
+    this.placeholder = 'missing_hole, mouse_bite, spur, short...';
 });
+
+clsInput.addEventListener('blur', function() {
+    this.placeholder = 'Например: missing_hole, mouse_bite, spur, short';
+});
+
+function previewImages(event) {
+    const preview = document.getElementById("preview");
+    preview.innerHTML = "";
+
+    [...event.target.files].forEach(file => {
+        const reader = new FileReader();
+        reader.onload = e => {
+            const col = document.createElement("div");
+            col.className = "col-6 col-md-4";
+
+            col.innerHTML = `
+                <div class="card shadow-sm">
+                    <img src="${e.target.result}" class="img-fluid rounded">
+                    <div class="card-body p-2">
+                        <small class="text-muted">${file.name}</small>
+                    </div>
+                </div>
+            `;
+            preview.appendChild(col);
+        };
+        reader.readAsDataURL(file);
+    });
+}
